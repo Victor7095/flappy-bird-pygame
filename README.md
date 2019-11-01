61181aa2549de878d0685d2500f6cecf03b0260e
Victor Yan Pereira e Lima <vypl.snf19@uea.edu.br>
2019-11-01 14:41:23 -0400

    background em movimento

--- README.md
+++ README.md
@@ -7,3 +7,8 @@
 - [Vinicius Soares da Costa](https://github.com/Vinicius-Soares)
 - [Vanessa de Souza Câmara](https://github.com/vanessadcamara)
 - [Williams da Silva Gomes](https://github.com/WilliamsGomes1)
+
+## Conteúdo
+
+- [Plano de fundo estático](https://www.pinterest.it/pin/303430093618641114/)
+- [Chão em movimento](https://pngimage.net/flappy-bird-background-png-3/)
--- main.py
+++ main.py
@@ -1,25 +1,49 @@
 """ Flappy Bird """
-import pygame
+import pygame as pg
+
+screen, clock, background, ground = [None]*4
+speed = 30
+ground_x = ground_x2 = 0
+running = False
+
+
+def redraw():
+    """Redesenha a tela """
+    screen.blit(background, (0, 0))
+    screen.blit(ground, (ground_x, 790))
+    screen.blit(ground, (ground_x2, 790))
+    pg.display.update()
 
 
 def main():
     """Main function"""
-    pygame.init()
-    logo = pygame.image.load("assets/icon.png")
-    pygame.display.set_icon(logo)
-    pygame.display.set_caption("Flappy Bird")
-    screen = pygame.display.set_mode((640, 980))
+    global screen, background, clock, ground, ground_x, ground_x2, running
+    pg.init()
+    pg.display.set_caption("Flappy Bird")
+    clock = pg.time.Clock()
 
-    image = pygame.image.load("assets/static-bg.jpg")
-    screen.blit(image, (0, 0))
+    screen = pg.display.set_mode((640, 980))
+    background = pg.image.load("assets/static-bg.jpg")
 
+    ground = pg.transform.scale(pg.image.load("assets/ground.png"), (772, 224))
+    ground_x = 0
+    ground_x2 = ground.get_width()
     running = True
-
     while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        redraw()
+
+        ground_x -= 8
+        ground_x2 -= 8
+        if ground_x < ground.get_width()*-1:
+            ground_x = ground.get_width()
+        if ground_x2 < ground.get_width()*-1:
+            ground_x2 = ground.get_width()
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
                 running = False
-        pygame.display.flip()
+
+        clock.tick(speed)
 
 
 if __name__ == "__main__":

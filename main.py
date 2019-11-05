5e278d24184e9e84f534909dd7374cd6b9412ce6
Victor Yan Pereira e Lima <vypl.snf19@uea.edu.br>
2019-11-05 16:42:08 -0400

    Tubos 1.0

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """ Flappy Bird """
 import pygame as pg
+from random import randint
 
 SPEED = 60
 SPRITES_DIR = "assets/images/"
@@ -9,10 +10,22 @@ AUDIO_DIR = "assets/audio/"
 def redraw():
     """Redesenha a tela """
     screen.blit(background, (0, 0))
+    for pipe in pipes:
+        screen.blit(pipe_sprite_inverted, (pipe["x"], pipe["upper"]))
+        screen.blit(pipe_sprite, (pipe["x"], pipe["lower"]))
+    screen.blit(player, (60, 260))
     screen.blit(ground, (ground_x, 400))
     screen.blit(ground, (ground_x2, 400))
-    screen.blit(player, (60, 260))
     pg.display.update()
+
+
+def generate_pipe():
+    pipe_height_key = randint(0, 10)
+    pipe = {
+        "upper": -pipe_height_key*20,
+        "lower": -pipe_height_key*20+390,
+        "x": ground.get_width()}
+    return pipe
 
 
 pg.init()
@@ -24,15 +37,19 @@ screen = pg.display.set_mode((288, 512))
 background = pg.image.load(
     SPRITES_DIR+"background-day.png").convert_alpha()
 player = pg.image.load(SPRITES_DIR+"redbird-midflap.png").convert_alpha()
-birdSprite = [SPRITES_DIR+"redbird-upflap.png", 
-              SPRITES_DIR+"redbird-midflap.png", 
+birdSprite = [SPRITES_DIR+"redbird-upflap.png",
+              SPRITES_DIR+"redbird-midflap.png",
               SPRITES_DIR+"redbird-downflap.png"]
 sprite = 0
 x = 1
 ground = pg.image.load(SPRITES_DIR+"base.png").convert_alpha()
+pipe_sprite = pg.image.load(SPRITES_DIR+"pipe-green.png").convert_alpha()
+pipe_sprite_inverted = pg.transform.flip(
+    pg.image.load(SPRITES_DIR+"pipe-green.png").convert_alpha(), False, True)
 
 ground_x = 0
 ground_x2 = ground.get_width()
+pipes = []
 
 running = True
 while running:
@@ -48,6 +65,16 @@ while running:
         player = pg.image.load(birdSprite[sprite])
         sprite = (sprite + 1) % len(birdSprite)
         x = 0
+
+    for pipe in pipes:
+        pipe["x"] -= 4
+
+    if len(pipes) > 0 and pipes[0]["x"] <= ground.get_width()*-1:
+        pipes.pop(0)
+        pipes.append(generate_pipe())
+    elif len(pipes) == 0:
+        pipes.append(generate_pipe())
+
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False

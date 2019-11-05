709a43b2f2f3392d058e88184fb440bb2bc65db0
Victor Yan Pereira e Lima <vypl.snf19@uea.edu.br>
2019-11-05 11:36:00 -0400

    Ajustando dimesões

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import pygame as pg
 
 screen, clock, background, ground, player = [None]*5
-speed = 30
+SPEED = 60
 ground_x = ground_x2 = 0
 running = False
 
@@ -13,9 +13,9 @@ AUDIO_DIR = "assets/audio/"
 def redraw():
     """Redesenha a tela """
     screen.blit(background, (0, 0))
-    screen.blit(ground, (ground_x, 790))
-    screen.blit(ground, (ground_x2, 790))
-    screen.blit(player, (100, 450))
+    screen.blit(ground, (ground_x, 400))
+    screen.blit(ground, (ground_x2, 400))
+    screen.blit(player, (60, 260))
     pg.display.update()
 
 
@@ -26,23 +26,22 @@ def main():
     pg.display.set_caption("Flappy Bird")
     clock = pg.time.Clock()
 
-    screen = pg.display.set_mode((640, 980))
-    background = pg.image.load(SPRITES_DIR+"static-bg.jpg")
+    screen = pg.display.set_mode((288, 512))
 
-    ground = pg.transform.scale(pg.image.load(
-        SPRITES_DIR+"ground.png"), (772, 224))
+    background = pg.image.load(
+        SPRITES_DIR+"background-day.png").convert_alpha()
+    player = pg.image.load(SPRITES_DIR+"redbird-midflap.png").convert_alpha()
+    ground = pg.image.load(SPRITES_DIR+"base.png").convert_alpha()
+
     ground_x = 0
     ground_x2 = ground.get_width()
-
-    player = pg.transform.scale(pg.image.load(
-        SPRITES_DIR+"redbird-midflap.png"), (102, 72))
 
     running = True
     while running:
         redraw()
 
-        ground_x -= 8
-        ground_x2 -= 8
+        ground_x -= 4
+        ground_x2 -= 4
         if ground_x < ground.get_width()*-1:
             ground_x = ground.get_width()
         if ground_x2 < ground.get_width()*-1:
@@ -52,7 +51,7 @@ def main():
             if event.type == pg.QUIT:
                 running = False
 
-        clock.tick(speed)
+        clock.tick(SPEED)
 
 
 if __name__ == "__main__":

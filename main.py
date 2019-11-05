0f3961c23e58c9c4487bbfa4b2667a4f2cf4b57b
Vinicius-Soares <vsdc.snf19@uea.edu.br>
2019-11-05 12:18:38 -0400

    animação passaro (ou quase)

--- main.py
+++ main.py
@@ -24,6 +24,11 @@ screen = pg.display.set_mode((288, 512))
 background = pg.image.load(
     SPRITES_DIR+"background-day.png").convert_alpha()
 player = pg.image.load(SPRITES_DIR+"redbird-midflap.png").convert_alpha()
+birdSprite = [SPRITES_DIR+"redbird-upflap.png", 
+              SPRITES_DIR+"redbird-midflap.png", 
+              SPRITES_DIR+"redbird-downflap.png"]
+sprite = 0
+x = 1
 ground = pg.image.load(SPRITES_DIR+"base.png").convert_alpha()
 
 ground_x = 0
@@ -32,14 +37,17 @@ ground_x2 = ground.get_width()
 running = True
 while running:
     redraw()
-
+    x += 1
     ground_x -= 4
     ground_x2 -= 4
     if ground_x <= ground.get_width()*-1:
         ground_x = ground.get_width()
     if ground_x2 <= ground.get_width()*-1:
         ground_x2 = ground.get_width()
-
+    if x == 10:
+        player = pg.image.load(birdSprite[sprite])
+        sprite = (sprite + 1) % len(birdSprite)
+        x = 0
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False

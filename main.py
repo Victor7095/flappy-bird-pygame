b7a79a6a6402c58c8e1f4d7b3489df828ee73f27
Victor Yan Pereira e Lima <vypl.snf19@uea.edu.br>
2019-11-06 14:08:10 -0400

    Game Over

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ screen = pg.display.set_mode((288, 512))
 
 background = pg.image.load(
     SPRITES_DIR+"background-day.png").convert_alpha()
-menu = pg.image.load(SPRITES_DIR+"message.png").convert_alpha()
+menu_sp = pg.image.load(SPRITES_DIR+"message.png").convert_alpha()
+game_over_sp = pg.image.load(SPRITES_DIR+"gameover.png").convert_alpha()
 ground = pg.image.load(SPRITES_DIR+"base.png").convert_alpha()
 pipe_sprite = pg.image.load(SPRITES_DIR+"pipe-green.png").convert_alpha()
 pipe_sprite_inverted = pg.transform.flip(
@@ -26,13 +27,42 @@ birdSprite = [SPRITES_DIR+"redbird-upflap.png",
               SPRITES_DIR+"redbird-midflap.png"]
 ground = pg.image.load(SPRITES_DIR+"base.png").convert_alpha()
 
+ground_x = 0
+ground_x2 = ground.get_width()
+
+pipes = []
+
 running = True
+
+
+# Desenho do chão
+def redraw_ground():
+    screen.blit(ground, (ground_x, 400))
+    screen.blit(ground, (ground_x2, 400))
+
+
+# Desenho dos tubos
+def redraw_pipes():
+    for pipe in pipes:
+        screen.blit(pipe_sprite_inverted, (pipe["x"], pipe["upper"]))
+        screen.blit(pipe_sprite, (pipe["x"], pipe["lower"]))
 
 
 def redraw_menu():
     screen.blit(background, (0, 0))
-    screen.blit(menu, ((background.get_width() - menu.get_width())/2,
-                       (background.get_height() - menu.get_height())/2))
+    screen.blit(menu_sp, ((background.get_width() - menu_sp.get_width())/2,
+                          (background.get_height() - menu_sp.get_height())/2))
+
+
+def redraw_game_over(player, player_position):
+    screen.blit(background, (0, 0))
+    redraw_pipes()
+    screen.blit(game_over_sp,
+                ((background.get_width() - menu_sp.get_width())/2,
+                 (background.get_height() - menu_sp.get_height())/2))
+
+    redraw_ground()
+    screen.blit(player, (60, player_position))
     pg.display.update()
 
 
@@ -45,20 +75,33 @@ def generate_pipe():
     return pipe
 
 
+def move_ground():
+    global ground_x, ground_x2
+    ground_x -= 4
+    ground_x2 -= 4
+    if ground_x <= ground.get_width()*-1:
+        ground_x = ground.get_width()
+    if ground_x2 <= ground.get_width()*-1:
+        ground_x2 = ground.get_width()
+
+
 def stop():
-    global running
-    running = False
+    pg.quit()
 
 
 def game_menu():
     while True:
         redraw_menu()
+        redraw_ground()
+        move_ground()
+        pg.display.update()
+
         for event in pg.event.get():
 
             # Sair ao clicar no X ou pressionar ESC
             if event.type == QUIT or (event.type == KEYDOWN and
                                       event.key == K_ESCAPE):
-                pg.quit()
+                stop()
 
             if event.type == KEYDOWN and (event.key == K_SPACE or
                                           event.key == K_UP):
@@ -73,28 +116,22 @@ def main_game():
     player = original_player
     player_position = 60
     sprite = angle = timer1 = timer2 = y_speed = 0
-
-    ground_x = 0
-    ground_x2 = ground.get_width()
-
     DidPlayerHitPipe = False
 
-    pipes = [generate_pipe()]
+    pipes.clear()
+    pipes.append(generate_pipe())
 
     while True:
         # Redesenhando a tela
         screen.blit(background, (0, 0))
-        for pipe in pipes:
-            screen.blit(pipe_sprite_inverted, (pipe["x"], pipe["upper"]))
-            screen.blit(pipe_sprite, (pipe["x"], pipe["lower"]))
-        screen.blit(ground, (ground_x, 400))
-        screen.blit(ground, (ground_x2, 400))
+        redraw_pipes()
+        redraw_ground()
         screen.blit(player, (60, player_position))
         pg.display.update()
 
         timer1 += 1
         timer2 += 1
-        
+
         # Animação de queda
         if timer2 == 2:
             timer2 = 0
@@ -114,23 +151,19 @@ def main_game():
         # condição de morte pelos tubos
         for pipe in pipes:
             if 30 <= pipe["x"] <= 90 and (
-            pipe["lower"] - 35 <= player_position or 
-            pipe["upper"] + 310 >= player_position):
+                pipe["lower"] - 35 <= player_position or
+                    pipe["upper"] + 310 >= player_position):
                 DidPlayerHitPipe = True
 
-        # Movimentação do background
-        if DidPlayerHitPipe == False:
-            ground_x -= 4
-            ground_x2 -= 4
-        if ground_x <= ground.get_width()*-1:
-            ground_x = ground.get_width()
-        if ground_x2 <= ground.get_width()*-1:
-            ground_x2 = ground.get_width()
+        if not DidPlayerHitPipe:
+            move_ground()
 
-        # Movimentação dos tubos
-        if DidPlayerHitPipe == False:
+            # Movimentação dos tubos
             for pipe in pipes:
-                pipe["x"] -= 3
+                pipe["x"] -= 4
+
+        elif DidPlayerHitPipe and player_position >= 375:
+            return (player, player_position)
 
         # Geração do próximo tubo
         if pipes[0]["x"] <= background.get_width()*-1:
@@ -139,18 +172,40 @@ def main_game():
             pipes.append(generate_pipe())
 
         for event in pg.event.get():
-            if event.type == pg.QUIT:
+            # Sair ao clicar no X ou pressionar ESC
+            if event.type == QUIT or (event.type == KEYDOWN and
+                                      event.key == K_ESCAPE):
                 stop()
             if event.type == pg.KEYDOWN:
                 # insira aqui a condiçao de vida do passaro
-                if event.key == pg.K_SPACE and 0 < player_position < 375 and DidPlayerHitPipe == False:
+                if(event.key == pg.K_SPACE and
+                   0 < player_position < 375 and
+                   not DidPlayerHitPipe):
                     y_speed = 7
                     angle = 60
 
         clock.tick(SPEED)
 
 
+def game_over(player, player_position):
+    while True:
+        redraw_game_over(player, player_position)
+
+        for event in pg.event.get():
+
+            # Sair ao clicar no X ou pressionar ESC
+            if event.type == QUIT or (event.type == KEYDOWN and
+                                      event.key == K_ESCAPE):
+                stop()
+
+            if event.type == KEYDOWN and (event.key == K_SPACE or
+                                          event.key == K_UP):
+                return
+
+        clock.tick(SPEED)
+
+
 while running:
     game_menu()
-    main_game()
-    clock.tick(SPEED)
+    [player, player_position] = main_game()
+    game_over(player, player_position)

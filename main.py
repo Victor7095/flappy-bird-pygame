def6fb4964ced5799642aa92d6856e4c7aa304c2
Victor Yan Pereira e Lima <vypl.snf19@uea.edu.br>
2019-11-07 00:13:21 -0400

    Pontuação corrigida

--- main.py
+++ main.py
@@ -21,21 +21,21 @@ ground = pg.image.load(SPRITES_DIR+"base.png").convert_alpha()
 pipe_sprite = pg.image.load(SPRITES_DIR+"pipe-green.png").convert_alpha()
 pipe_sprite_inverted = pg.transform.flip(
     pg.image.load(SPRITES_DIR+"pipe-green.png").convert_alpha(), False, True)
-score = pg.image.load(SPRITES_DIR+"0.png").convert_alpha()
 birdSprite = [SPRITES_DIR+"redbird-upflap.png",
               SPRITES_DIR+"redbird-midflap.png",
               SPRITES_DIR+"redbird-downflap.png",
               SPRITES_DIR+"redbird-midflap.png"]
-scoreSprite = [SPRITES_DIR+"0.png",
-               SPRITES_DIR+"1.png",
-               SPRITES_DIR+"2.png",
-               SPRITES_DIR+"3.png",
-               SPRITES_DIR+"4.png",
-               SPRITES_DIR+"5.png",
-               SPRITES_DIR+"6.png",
-               SPRITES_DIR+"7.png",
-               SPRITES_DIR+"8.png",
-               SPRITES_DIR+"9.png"]
+scoreSprite = [pg.image.load(SPRITES_DIR+"0.png"),
+               pg.image.load(SPRITES_DIR+"1.png"),
+               pg.image.load(SPRITES_DIR+"2.png"),
+               pg.image.load(SPRITES_DIR+"3.png"),
+               pg.image.load(SPRITES_DIR+"4.png"),
+               pg.image.load(SPRITES_DIR+"5.png"),
+               pg.image.load(SPRITES_DIR+"6.png"),
+               pg.image.load(SPRITES_DIR+"7.png"),
+               pg.image.load(SPRITES_DIR+"8.png"),
+               pg.image.load(SPRITES_DIR+"9.png")]
+score_sp = [scoreSprite[0]]
 ground = pg.image.load(SPRITES_DIR+"base.png").convert_alpha()
 
 ground_x = 0
@@ -63,9 +63,16 @@ def redraw_menu():
     screen.blit(background, (0, 0))
     screen.blit(menu_sp, ((background.get_width() - menu_sp.get_width())/2,
                           (background.get_height() - menu_sp.get_height())/2))
+    redraw_ground()
+    pg.display.update()
+
 
 def redraw_score():
-    screen.blit(score, (125, 10))
+    number_x = (background.get_width()-24*len(score_sp))/2
+    for number in score_sp:
+        screen.blit(number, (number_x, 10))
+        number_x += 24
+
 
 def redraw_game_over(player, player_position):
     screen.blit(background, (0, 0))
@@ -105,9 +112,8 @@ def stop():
 def game_menu():
     while True:
         redraw_menu()
-        redraw_ground()
+
         move_ground()
-        pg.display.update()
 
         for event in pg.event.get():
 
@@ -116,6 +122,7 @@ def game_menu():
                                       event.key == K_ESCAPE):
                 stop()
 
+            # Iniciar jogo
             if event.type == KEYDOWN and (event.key == K_SPACE or
                                           event.key == K_UP):
                 return
@@ -124,14 +131,15 @@ def game_menu():
 
 
 def main_game():
+    global score_sp
     original_player = pg.image.load(
         SPRITES_DIR+"redbird-downflap.png").convert_alpha()
-    score = pg.image.load(
-        SPRITES_DIR+"0.png").convert_alpha()
     player = original_player
     player_position = 60
     sprite = angle = timer1 = timer2 = y_speed = score_counter = 0
-    DidPlayerHitPipe = False
+    has_player_colided = False
+
+    score_sp = [pg.image.load(SPRITES_DIR+"0.png").convert_alpha()]
 
     pipes.clear()
     pipes.append(generate_pipe())
@@ -169,35 +177,48 @@ def main_game():
             if 30 <= pipe["x"] <= 90 and (
                 pipe["lower"] - 35 <= player_position or
                     pipe["upper"] + 310 >= player_position):
-                DidPlayerHitPipe = True
+                has_player_colided = True
 
-        if not DidPlayerHitPipe:
+        # Se o jogador estiver vivo move chão e tubos
+        if not has_player_colided:
             move_ground()
 
             # Movimentação dos tubos
             for pipe in pipes:
                 pipe["x"] -= 4
 
-        elif DidPlayerHitPipe and player_position >= 375:
+        # Se o jogador estiver morto e no chão vai pro gameover
+        elif has_player_colided and player_position >= 375:
             return (player, player_position)
 
-        # Geração do próximo tubo / pontuaçao
-        if pipes[0]["x"] <= background.get_width()*-1:
+        # Geração do próximo tubo
+        if pipes[0]["x"] <= pipe_sprite.get_width()*-1:
             pipes.pop(0)
         if pipes[-1]["x"] == 108:
             pipes.append(generate_pipe())
-            score_counter = (score_counter +1) % len(scoreSprite)
-            score = pg.image.load(scoreSprite[score_counter])
+
+        # Pontuação
+        if pipes[0]["x"] == 56:
+            score_counter += 1
+            score_sp.clear()
+            score_aux = score_counter
+            while score_aux > 0:
+                score_sp.append(scoreSprite[score_aux % 10])
+                score_sp.reverse()
+                score_aux = int(score_aux/10)
 
         for event in pg.event.get():
+
             # Sair ao clicar no X ou pressionar ESC
             if event.type == QUIT or (event.type == KEYDOWN and
                                       event.key == K_ESCAPE):
                 stop()
+
+            # Salto
             if event.type == pg.KEYDOWN:
                 if(event.key == pg.K_SPACE and
                    0 < player_position < 375 and
-                   not DidPlayerHitPipe):
+                   not has_player_colided):
                     y_speed = 7
                     angle = 60
 
@@ -215,6 +236,7 @@ def game_over(player, player_position):
                                       event.key == K_ESCAPE):
                 stop()
 
+            # Voltar ao menu
             if event.type == KEYDOWN and (event.key == K_SPACE or
                                           event.key == K_UP):
                 return
